6a750b563585920e251b296869af3b11c8f591f7
tanzorr <alexx1984@ukr.net>
2019-11-04 18:59:27 +0200

    chec 23

--- src/componets/Login/login.js
+++ src/componets/Login/login.js
@@ -1,22 +1,27 @@
 import React from  'react';
 import {Field, reduxForm} from "redux-form";
 import {Input} from "../comon/FormsControl/FormsControls";
-import {required} from "../../utils/validators";
+import {required, maxLengthCriator} from "../../utils/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
 import {Redirect} from "react-router-dom";
+import style from "../comon/FormsControl/FormsControls.module.css";
+
+
 
 const LoginForm = (props)=>{
+    let maxlength15 = maxLengthCriator(16);
     return (
+
         <form onSubmit={props.handleSubmit}>
             <div>
                 <Field  placeholder={"Email"}
-                        validate={[required]}
+                        validate={[required, maxlength15 ]}
                         name={"email"} component={Input}/>
             </div>
             <div>
                 <Field placeholder={"Password"}
-                       validate={[required]}
+                       validate={[required, ]}
                        name={"password"}
                        type={"password"}
                        component={Input}/>
@@ -26,6 +31,11 @@ const LoginForm = (props)=>{
                        validate={[required]}
                        name={"rememberMe"} type="checkbox"/> remember me
             </div>
+            {props.error &&
+            <div className={style.formSummaryError}>
+
+            </div>
+            }
             <div>
                 <button>Login</button>
             </div>
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,4 +1,5 @@
 import {authAPI} from "../api/api";
+import {stopSubmit} from "redux-form";
 
 const SET_USERS_DATA = 'SET-USERS-DATA';
 const UNFOLLOW = 'UNFOLLOW';
@@ -39,9 +40,13 @@ export const getAuthUserData =() =>(dispatch)=>{
 
 
 export const login =(email,password,rememberMe) =>(dispatch)=>{
+
     authAPI.login(email,password,rememberMe).then(response=>{
         if(response.data.resultCode === 0){
          dispatch(getAuthUserData())
+        }else{
+            let message = response.data.message.length > 0 ? response.data.message[0] : "Some error"
+            dispatch(stopSubmit("login",({error:"Email is wrong"})));
         }
     });
 };
